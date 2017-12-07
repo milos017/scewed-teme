441cb1dd1dd6f6fc49e1516ad21be22aae6e94ab
Milos Stojiljkovic <milos017ffm@yahoo.com>
2017-12-07 16:12:18 +0100

    footer

--- js/main.js
+++ js/main.js
@@ -1,22 +1,75 @@
 $(document).ready(function(){
-    
-$(window).scroll(function(){
-   var scroll = $(window).scrollTop();
-   if(scroll > 150){
-       $('header').addClass('py-2');
-   }else{
-       $('header').removeClass('py-2');
-   }
-});
 
-$('.navbar-nav a').click(function(){
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 2000);
+$(window).scroll(function(){
+var scroll = $(window).scrollTop();
+        if (scroll > 150){
+$('header').addClass('py-2');
+} else{
+$('header').removeClass('py-2');
+}
 });
-    
-    if($('bxslider').length > 0){
-        $('.lead-slider').bxslider();
-    };
-    
+        $('.navbar-nav a').click(function(){
+$('html, body').animate({
+scrollTop: $($.attr(this, 'href')).offset().top
+}, 2000);
+        });
+        if ($('bxslider').length > 0){
+$('.lead-slider').bxslider();
+};
+        function animation(){
+        var windowHight = $(window).height();
+                var scroll = $(window).scrollTop();
+                $('animation').each(function(){
+        var pozicija = $(this).offset().top();
+                var animacija = $(this).attr('data-animation');
+                if (pozicija < scroll + windowHight - 100){
+        $(this).addClass(animacija);
+        }
+        });
+        }
+
+animation();
+        $(window).scroll(function(){
+animation();
+        if ($('.owl-carousel').length > 0){
+$('.team-slider').owlCarousel({
+loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+        0:{
+        items:1,
+        },
+                480:{
+                items:2,
+                },
+                768:{
+                items:3,
+                },
+                992:{
+                items:4,
+                },
+        }
 });
+};
+
+$(window).scroll(function(){
+var wh = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.count').each(function () {
+var position = $(this).offset().top;
+        if (position < wh + scroll - 100){
+$(this).prop('Counter', 0).animate({
+Counter: $(this).text()
+}, {
+duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+        $(this).text(Math.ceil(now));
+        }
+});
+}
+
+});
+});
+        });
